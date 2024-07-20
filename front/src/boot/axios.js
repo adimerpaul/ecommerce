71a5b6d0c7c9f0bb5809464129afc0fc46d780c7
adimerpaul <adimer101@gmail.com>
2024-07-20 05:35:15 -0400

    funcioando carousel

--- front/src/boot/axios.js
+++ front/src/boot/axios.js
@@ -1,5 +1,6 @@
 import { boot } from 'quasar/wrappers'
 import axios from 'axios'
+import * as url from "url";
 
 // Be careful when using SSR for cross-request state pollution
 // due to creating a Singleton instance here;
@@ -7,18 +8,19 @@ import axios from 'axios'
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: 'https://api.example.com' })
+const api = axios.create({ baseURL: import.meta.env.VITE_API_URL })
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
 
-  app.config.globalProperties.$axios = axios
+  app.config.globalProperties.$axios = axios.create({ baseURL: import.meta.env.VITE_API_URL })
   // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
   //       so you won't necessarily have to import axios in each vue file
 
   app.config.globalProperties.$api = api
+  app.config.globalProperties.$url  = import.meta.env.VITE_API_URL
   // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
   //       so you can easily perform requests against your app's API
 })
 
-export { api }
+export { api, axios, url }
--- front/src/pages/IndexPage.vue
+++ front/src/pages/IndexPage.vue
@@ -1,15 +1,59 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page>
+    <div class="row">
+      <div class="col-12 col-md-1"></div>
+      <div class="col-12 col-md-10">
+        <div class="q-pa-md">
+          <q-carousel
+            class="br-10"
+            animated
+            v-model="slide"
+            arrows
+            navigation
+            infinite
+            :height="$q.screen.lt.sm ? '150px' : '450px'"
+          >
+<!--            <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg" />-->
+<!--            <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />-->
+<!--            <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />-->
+<!--            <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" />-->
+            <q-carousel-slide
+              v-for="(carousel, index) in carousels"
+              :key="index"
+              :name="index"
+              :img-src="`${$url}../images/${carousel.imagen}`"
+            >
+            </q-carousel-slide>
+          </q-carousel>
+        </div>
+      </div>
+    </div>
+    <pre>{{carousels}}</pre>
   </q-page>
 </template>
 
-<script setup>
-defineOptions({
-  name: 'IndexPage'
-});
+<script>
+export default {
+  data() {
+    return {
+      slide: 0,
+      carousels: [],
+    };
+  },
+  mounted() {
+    this.carouselsGet();
+  },
+  methods: {
+    carouselsGet() {
+      this.$axios
+        .get("carousels")
+        .then((response) => {
+          this.carousels = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
