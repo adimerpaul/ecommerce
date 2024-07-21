ee6de99f4d3a8d9e6abcc12c0fed339d3d3cb3e1
adimerpaul <adimer101@gmail.com>
2024-07-21 06:34:39 -0400

    colocado de text y item

--- front/src/pages/Products/ProductDetail.vue
+++ front/src/pages/Products/ProductDetail.vue
@@ -1,20 +1,102 @@
 <template>
 <q-page>
-  <q-carousel
-    class="br-20"
-    animated
-    v-model="slide"
-    arrows
-    navigation
-    autoplay
-    infinite
-    :height="$q.screen.lt.sm ? '400px' : '500px'"
-  >
-    <q-carousel-slide :img-src="`${$url}../images/${product.imagen1}`" :name="0">
-    </q-carousel-slide>
-  </q-carousel>
-  <pre>{{`${$url}../images/${product.imagen1}` }}</pre>
-    <pre>{{product}}</pre>
+  <div class="row">
+    <div class="col-12 col-md-1"></div>
+    <div class="col-12 col-md-10">
+      <div class="row">
+        <div class="col-12 row items-center q-pt-md q-px-md">
+          <q-btn
+            round
+            size="12px"
+            unelevated
+            color="grey-3"
+            text-color="black"
+            icon="fa-solid fa-arrow-left"
+            aria-label="Back"
+            @click="$router.go(-1)"/>
+          <q-space/>
+          <q-btn-group flat>
+            <q-btn
+              round
+              text-color="black"
+              size="12px"
+              icon="fa-solid fa-heart"
+              aria-label="Like"
+              @click="favoritosClick"
+            />
+            <q-btn
+              round
+              text-color="black"
+              size="12px"
+              icon="fa-solid fa-search"
+              aria-label="Search"
+            />
+          </q-btn-group>
+        </div>
+        <div class="col-12">
+          <div class="row">
+            <div class="col-12 col-md-6" >
+              <q-carousel
+                class="br-20"
+                animated
+                v-model="slide"
+                arrows
+                navigation
+                autoplay
+                infinite
+                :height="$q.screen.lt.sm ? '400px' : '500px'"
+                control-color="primary"
+              >
+                <q-carousel-slide :img-src="`${$url}../images/${product.imagen1}`" :name="0" v-if="product.imagen1"/>
+                <q-carousel-slide :img-src="`${$url}../images/${product.imagen2}`" :name="1" v-if="product.imagen2"/>
+                <q-carousel-slide :img-src="`${$url}../images/${product.imagen3}`" :name="2" v-if="product.imagen3"/>
+                <q-carousel-slide :img-src="`${$url}../images/${product.imagen4}`" :name="3" v-if="product.imagen4"/>
+              </q-carousel>
+            </div>
+            <div class="col-12 col-md-6 q-pa-md">
+              <div class="text-bold text-h5 q-px-md">{{product.titulo}}</div>
+              <q-separator inset/>
+              <div class="row items-center">
+                <div class="q-pa-md">
+                  <q-icon name="fa-solid fa-star" color="grey"/>
+                  <span class="text-bold q-pl-xs "> {{product.category?.name}}</span>
+                  |
+                  <span class="text-bold q-pl-xs "> {{product.sub_category?.name}}</span>
+                </div>
+                <q-space/>
+                <q-badge color="green-2" text-color="green-7" :label="`Stock ${product?.stock}`" class="text-bold text-subtitle2" v-if="product.stock > 0"/>
+              </div>
+              <div class="text-bold text-h6 q-px-md">
+                <span class="text-blue-8 text-bold text-h5">${{product.precio}}</span>
+                <span class="text-grey-8 text-bold text-h5 line-through q-ml-md">  ${{product.precioAnterior}}</span>
+              </div>
+              <div class="q-px-md">
+                <q-scroll-area style="height: 180px;">
+                  <div class="text-grey-8" v-html="product.descripcion"></div>
+                </q-scroll-area>
+              </div>
+              <div class="q-px-md q-pt-md">
+                <q-radio v-model="radio" val="product.item1" class="q-pl-md" :label="product.item1" color="primary" dense v-if="product.item1"/>
+                <q-radio v-model="radio" val="product.item2" class="q-pl-md" :label="product.item2" color="primary" dense v-if="product.item2"/>
+                <q-radio v-model="radio" val="product.item3" class="q-pl-md" :label="product.item3" color="primary" dense v-if="product.item3"/>
+                <q-radio v-model="radio" val="product.item4" class="q-pl-md" :label="product.item4" color="primary" dense v-if="product.item4"/>
+                <q-radio v-model="radio" val="product.item5" class="q-pl-md" :label="product.item5" color="primary" dense v-if="product.item5"/>
+                <q-radio v-model="radio" val="product.item6" class="q-pl-md" :label="product.item6" color="primary" dense v-if="product.item6"/>
+                <q-radio v-model="radio" val="product.item7" class="q-pl-md" :label="product.item7" color="primary" dense v-if="product.item7"/>
+                <q-radio v-model="radio" val="product.item8" class="q-pl-md" :label="product.item8" color="primary" dense v-if="product.item8"/>
+                <q-radio v-model="radio" val="product.item9" class="q-pl-md" :label="product.item9" color="primary" dense v-if="product.item9"/>
+                <q-radio v-model="radio" val="product.item10" class="q-pl-md" :label="product.item10" color="primary" dense v-if="product.item10"/>
+              </div>
+              <div class="q-px-md q-pt-md">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-12 col-md-1"></div>
+  </div>
+  <pre>{{product}}</pre>
 </q-page>
 </template>
 <script>
@@ -25,12 +107,17 @@ export default {
       product_id: this.$route.params.id,
       product: {},
       slide: 0,
+      radio: '',
+      cantidad: 1,
     }
   },
   mounted() {
     this.productGet();
   },
   methods: {
+    favoritosClick() {
+      console.log('favoritosClick');
+    },
     async productGet() {
       try {
         const response = await this.$axios.get(`producto/${this.product_id}`);
