90e3ea1bac9c2e2592d837e3045973fc74019572
adimerpaul <adimer101@gmail.com>
2024-07-24 07:44:10 -0400

    recuperando proyectos

--- front/src/pages/Menu/ProductsPage.vue
+++ front/src/pages/Menu/ProductsPage.vue
@@ -1,7 +1,37 @@
 <template>
   <q-page>
     <div>
-      Products Page
+      <a>{{products}}</a>
     </div>
   </q-page>
 </template>
+<script>
+export default {
+  name: 'ProductsPage',
+  data () {
+    return {
+      products: [],
+      product: {},
+      categories: [],
+      loading: false,
+    }
+  },
+  mounted() {
+    this.getProducts()
+  },
+  methods: {
+    getProducts () {
+      this.loading = true
+      this.$axios.get('products')
+        .then(response => {
+          this.products = response.data
+        })
+        .catch(error => {
+          this.$alert.error(error.response.data.message)
+        }).finally(() => {
+          this.loading = false
+        })
+    }
+  },
+}
+</script>
